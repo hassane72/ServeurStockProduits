2a07a5b2ddbda67052ab09ffd8111923304465c5
HASSANE WANKOYE <hassaneabdou72@yahoo.fr>
2020-03-11 20:08:44 +0100

    first commit

---
+++ src/main/java/com/otimbi/demo/ServeurStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.otimbi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurStockProduitsApplication.class, args);
+	}
+
+}
